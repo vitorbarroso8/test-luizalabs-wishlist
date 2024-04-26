6970349867173942eb231ab737133eb104750699
Jose Vitor Barroso Pontes <jose.pontes@fazenda.mg.gov.br>
2024-04-26 16:56:03 -0300

    initial commit

---
+++ src/main/java/com/luizalabs/wishlist/TestLuizalabsWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.luizalabs.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLuizalabsWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestLuizalabsWishlistApplication.class, args);
+    }
+
+}
